577d14bc4cb49e8b7bae7ee2aadc2af37c9a26a8
wwwCrazy <407503201@qq.com>
2016-09-29 16:26:31 +0800

    简历文件
    简历文件

---
+++ cv-template/static/js/script.js
@@ -0,0 +1,18 @@
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+
+jQuery(document).ready(function($) {
+     $("*").removeAttr('contenteditable');
+
+});
